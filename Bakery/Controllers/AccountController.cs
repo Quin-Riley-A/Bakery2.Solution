f12cf734074de6e087d537c44343a1ddb8e14705
Quin-Riley-A <quin.asselin1@gmail.com>
2023-01-29 06:18:10 -0500

    include account login/registration controller

--- Bakery/Controllers/AccountController.cs
+++ Bakery/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace Bakery.Controllers
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RecipeBoxContext db)
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BakeryContext db)
     {
       _userManager = userManager;
       _signInManager = signInManager;
