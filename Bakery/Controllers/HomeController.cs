f399b2103f6820ef9cf28eb5f09cb22a2b60d717
Quin Asselin <quin.asselin1@gmail.com>
2023-02-06 23:26:19 -0500

    adjust from basic home controller setup

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,15 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+using Bakery.Models;
 
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakeryContext _db;
+
+    public HomeController(BakeryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
+    {        
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
+    }
+
+    public ActionResult About()
     {
       return View();
     }
-    
   }
 }
